a6d45683880fc58cc98f82c2592dc1ff0060344e
Jeff Lapp <lappjeff20@gmail.com>
2019-12-10 18:26:09 -0500

    Added proper redirect url, added conditions to returned data

--- routes/auth/authRoutes.js
+++ routes/auth/authRoutes.js
@@ -7,8 +7,6 @@ const scope = "user-read-private user-read-email";
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 
-console.log(process.env.REDIRECT_URI);
-
 router.get("/login", async (req, res) => {
 	const params = qs.stringify({
 		response_type: "code",
@@ -54,14 +52,14 @@ router.get("/callback", async (req, res) => {
 		const status = response.status;
 
 		res
-			.status(status || 200)
 			.cookie("spotifyAccessToken", accessToken, { maxAge: 34000 })
-			// .json({ accessToken, refreshToken });
-			.redirect("localhost:3000");
+			.cookie("spotifyRefreshToken", refreshToken)
+			.redirect("http://localhost:3000")
+			.status(status || 200);
 	} catch (err) {
-		res.status(err.response.status).json({
-			message: err.response.statusText,
-			error: err.response.data.error_description
+		res.status(err.response.status || 500).json({
+			message: err.response.statusText || "Error ",
+			error: err.response.data.error_description || "Unknown Error Occurred"
 		});
 	}
 });
