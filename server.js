70d85eaa62de0ea1b64b5e28a63fc41d65bb7351
Jeff Lapp <lappjeff20@gmail.com>
2019-12-14 16:26:00 -0500

    Added base route for home page

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use("/api/auth", authRouter);
 
 const port = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+	res.status(200).json({ message: `Server running on port ${port}` });
+});
+
 app.listen(port, () => {
 	console.info(`Server running on port ${port}`);
 });
